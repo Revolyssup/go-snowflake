3f3ccc62abcc5aed83c5983c4f0b6a8e2a83b5fd
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-06-25 19:11:18 +0530

    update test

--- idgen/idgent_test.go
+++ idgen/idgent_test.go
@@ -1,38 +1,21 @@
 package idgen
 
 import (
-	"sync"
 	"testing"
 )
 
 func TestIDGenTimeSortable(t *testing.T) {
-	worker := func(sf *Snowflake, wg *sync.WaitGroup, ch chan<- uint64) {
-		defer wg.Done()
-		ch <- sf.GetUUID()
-	}
 	sf := NewSnowflake(nil)
-	var wg sync.WaitGroup
-	ch := make(chan uint64, 1_000)
-	for range 1_000_000 {
-		wg.Add(1)
-		go worker(sf, &wg, ch)
-	}
-	var wg2 sync.WaitGroup
-	wg2.Add(1)
-	go func(ch <-chan uint64) {
-		defer wg2.Done()
-		var curr uint64
-		for val := range ch {
-			if val <= curr {
-				t.Error("Produced lower or equal value and not higher")
-				t.Fail()
-			}
+	var curr uint64
+	for range 1_000_0 {
+		new := sf.GetUUID()
+		if new <= curr {
+			t.Errorf("new value recieved %d for uuid is smaller or eq to previous %d", new, curr)
+			t.FailNow()
+		} else {
+			curr = new
 		}
-	}(ch)
-
-	wg.Wait()
-	close(ch)
-	wg2.Wait()
+	}
 }
 
 func BenchmarkIDGen(b *testing.B) {
